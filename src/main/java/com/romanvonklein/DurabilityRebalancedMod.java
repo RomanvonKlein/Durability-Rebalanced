2b687e6513a9927814538f66238bec3da0922b56
Roman Kravanja <roman.m.kravanja@student.fh-kiel.de>
2024-05-18 19:03:17 +0200

    Fixed mixins.

--- src/main/java/com/romanvonklein/DurabilityRebalancedMod.java
+++ src/main/java/com/romanvonklein/DurabilityRebalancedMod.java
@@ -18,6 +18,5 @@ public class DurabilityRebalancedMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
 	}
 }
